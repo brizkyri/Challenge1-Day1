47fc02b49b925e908ceebb963917372d6159933d
brizkyri <61369206+brizkyri@users.noreply.github.com>
2024-02-02 17:08:26 +0700

    myPHPProject

---
+++ Day5/myphpproject/ifelse.php
@@ -0,0 +1,15 @@
+<?php
+    $nilai = int;
+    $grade = string;
+
+    if ($nilai >= 85 && <=100){
+        echo "NIlai Anda adalah" . $nilai ;
+        echo "Grade Anda adalah A";
+    }
+    else if($niali >= 75 && <=84) {
+        echo "NIlai Anda adalah" . $nilai ;
+        echo "Grade Anda adalah B";
+    else {
+        echo "Masukkan Nilai";
+    }
+    }
---
+++ Day5/myphpproject/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Project</title>
+</head>
+<body>
+
+<?php
+    echo "Hello, World!";
+?>
+
+</body>
+</html>
